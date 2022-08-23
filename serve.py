13646234ef14cb2b94cd360bc278bffb0e54af29
Brandon Corfman <brandon.c.corfman.civ@army.mil>
2022-08-23 14:55:46 -0400

    FIX: working copy of API for both Unix and Windows platforms

--- serve.py
+++ serve.py
@@ -1,8 +1,11 @@
-import checkers
-from threading import Thread
-from waitress import serve
+from contextlib import contextmanager
+from checkers import start_server
+from multiprocessing import Process
 
 
-def waitress_serve_api():
-    thread = Thread(target=serve, args=(checkers.__hug_wsgi__,), kwargs={'host': "127.0.0.1", 'port': 8000})
-    thread.start()
+@contextmanager
+def serve_api():
+    proc = Process(target=start_server)
+    proc.start()
+    yield proc
+    proc.terminate()
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,10 @@
-from 
-from api import valid_moves
+from serve import serve_api
+from playwright.sync_api import sync_playwright
 
+def test_valid_moves_http():
+    with serve_api() as _:
+        with sync_playwright() as p:
+            context = p.request.new_context()
+            response = context.get("http://localhost:8000/valid_moves?black_men=11,15&black_kings=19&white_men=30,31&white_kings=29&player_to_move=black")
+            assert response.ok
+            assert response.json()["output"] == "1,2,3"
