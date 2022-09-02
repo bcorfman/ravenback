46a742f9adb3acb7f2a79ffe3700358f9ec0c45f
Brandon Corfman <bcorfman@fastmail.fm>
2022-09-01 23:30:05 -0400

    FEATURE: added valid ranges to integer parameters

--- checkers.py
+++ checkers.py
@@ -9,10 +9,10 @@ app = FastAPI()
 async def legal_moves(to_move: str=Query(default=Required, title="Player to move", 
                                          regex=r"\b(black|white)\b",
                                          description="black or white are valid selections"),
-                      bm: list[int]=Query(default=None, title="Black men", description="Values 1-32"), 
-                      bk: list[int]=Query(default=None, title="Black kings", description="Values 1-32"), 
-                      wm: list[int]=Query(default=None, title="White men", description="Values 1-32"), 
-                      wk: list[int]=Query(default=None, title="White kings", description="Values 1-32")):
+                      bm: list[int]=Query(default=None, title="Black men", description="Squares with black men", ge=1, le=32), 
+                      bk: list[int]=Query(default=None, title="Black kings", description="Squares with black kings", ge=1, le=32), 
+                      wm: list[int]=Query(default=None, title="White men", description="Squares with white men", ge=1, le=32), 
+                      wk: list[int]=Query(default=None, title="White kings", description="Squares with white kings", ge=1, le=32)):
         return {"output": "1,2,3"}
 
 
