bfe5584155cb431dff63fa96ed0193bb02f99374
Brandon Corfman <brandon.c.corfman.civ@army.mil>
2022-09-02 14:10:54 -0400

    FIX: improved legal_moves tests for validation errors

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,3 +10,17 @@ async def test_legal_moves():
         response = await ac.get("/legal_moves/?to_move=black")
     assert response.status_code == 200
     assert response.json() == {"output": "1,2,3"}
+
+
+@pytest.mark.anyio
+async def test_legal_moves_validation_error_illegal_player():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/legal_moves/?to_move=red")
+    assert response.status_code == 422
+
+
+@pytest.mark.anyio
+async def test_legal_moves_validation_error_illegal_range():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/legal_moves/?to_move=black&bm=5&bk=33")
+    assert response.status_code == 422
