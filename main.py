52adcb146b723a66e6b0e91b6f77766c832c5120
Brandon Corfman <bcorfman@fastmail.fm>
2023-07-16 00:19:45 -0400

    Try list[int] validation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Query
-from typing import Annotated
+from typing import Annotated, List
 
 app = FastAPI()
 
@@ -7,8 +7,12 @@ app = FastAPI()
 @app.get("/legal_moves/")
 async def legal_moves(to_move: Annotated[str, Query(title="Player to move", pattern=r"\b(black|white)\b",
                                                     description="black or white are valid selections")],
-                      bm: Annotated[list[int], Query(title="Black men", description="Squares with black men")], 
-                      bk: Annotated[list[int], Query(title="Black kings", description="Squares with black kings")], 
-                      wm: Annotated[list[int], Query(title="White men", description="Squares with white men")], 
-                      wk: Annotated[list[int], Query(title="White kings", description="Squares with white kings")]):
-        return {"output": "1,2,3"}
+                      bm: Annotated[List[int], Query(title="Black men", description="Squares with black men", 
+                                                     ge=1, le=32)], 
+                      bk: Annotated[List[int], Query(title="Black kings", description="Squares with black kings",
+                                                     ge=1, le=32)], 
+                      wm: Annotated[List[int], Query(title="White men", description="Squares with white men",
+                                                     ge=1, le=32)], 
+                      wk: Annotated[List[int], Query(title="White kings", description="Squares with white kings",
+                                                     ge=1, le=32)]):
+    return {"output": "1,2,3"}
