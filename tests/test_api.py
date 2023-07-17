b845d7e1306d9559295724e8f7ff056a70ceaf76
Brandon Corfman <bcorfman@fastmail.fm>
2023-07-17 14:28:59 -0400

    Added out of range and overlap tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -24,3 +24,36 @@ def test_legal_moves_repeated_values_white_kings():
         '/legal_moves/?to_move=white&bk=9&bk=27&wk=15&wk=15&wk=19')
     assert response.status_code == 422  # unprocessable content
     assert response.json() == {"detail": "Repeated values for white kings"}
+
+
+def test_legal_moves_repeated_values_white_kings():
+    client = TestClient(app)
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=9&bk=27&wk=15&wk=15&wk=19')
+    assert response.status_code == 422  # unprocessable content
+    assert response.json() == {"detail": "Repeated values for white kings"}
+
+
+def test_overlapping_checkers():
+    client = TestClient(app)
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=9&bk=27&wk=15&wk=15&wk=27')
+    assert response.status_code == 422  # unprocessable content
+    assert response.json() == {"detail": "Overlapping checker values"}
+
+
+def test_checker_values_out_of_range():
+    client = TestClient(app)
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=9&bk=27&wk=15&wk=15&wk=19&wk=0')
+    assert response.status_code == 422  # unprocessable content
+    assert response.json() == {
+        "detail": "Valid checker values range from 1-32"
+    }
+
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=9&bk=27&wk=15&wk=15&wk=19&wk=33')
+    assert response.status_code == 422  # unprocessable content
+    assert response.json() == {
+        "detail": "Valid checker values range from 1-33"
+    }
