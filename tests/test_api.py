3cc3466b65ee0b08091855604ba46e24a13432e5
Brandon Corfman <bcorfman@fastmail.fm>
2023-07-17 03:18:29 -0400

    Updated tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,23 +1,26 @@
-import pytest
-from httpx import AsyncClient
+from fastapi.testclient import TestClient
 
 from main import app
 
 
-async def test_legal_moves():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/legal_moves/?to_move=black")
+def test_legal_moves_success_code():
+    client = TestClient(app)
+    # start by clearing out existing high scores
+    response = client.get('/legal_moves/?to_move=white&bk=9&bk=27&wk=10&wk=15&wk=19')
     assert response.status_code == 200
-    assert response.json() == {"output": "1,2,3"}
 
 
-async def test_legal_moves_validation_error_illegal_player():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/legal_moves/?to_move=red")
-    assert response.status_code == 422
+def test_legal_moves_repeated_values_black_kings():
+    client = TestClient(app)
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=27&bk=27&wk=10&wk=15&wk=19')
+    assert response.status_code == 422  # unprocessable content
+    assert response.text == "Repeated values for black kings"
 
 
-async def test_legal_moves_validation_error_illegal_range():
-    async with AsyncClient(app=app, base_url="http://test") as ac:
-        response = await ac.get("/legal_moves/?to_move=black&bm=5&bk=33")
-    assert response.status_code == 422
+def test_legal_moves_repeated_values_white_kings():
+    client = TestClient(app)
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=9&bk=27&wk=15&wk=15&wk=19')
+    assert response.status_code == 422  # unprocessable content
+    assert response.text == "Repeated values for white kings"
