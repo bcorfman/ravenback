e12f2897a7ac2b067027c38bf29361c1c22f520f
Brandon Corfman <bcorfman@fastmail.fm>
2023-07-18 12:48:59 +0000

    Fixed breeches test

--- main.py
+++ main.py
@@ -11,17 +11,28 @@ app = FastAPI()
 
 # example - http://localhost:8000/legal_moves/?to_move=black&bm=11&bm=15&bk=19&bk=4&wm=30&wm=31&wk=29")
 @app.get("/legal_moves/")
-async def legal_moves(to_move: Annotated[str, Query(title="Player to move", pattern=r"\b(black|white)\b",
-                                                    description="black or white are valid selections")],
-                      bm: Annotated[List[int], Query(title="Black men",
-                                                     description="Squares with black men")] = [],
-                      bk: Annotated[List[int], Query(title="Black kings", description="Squares with black kings")] = [],
-                      wm: Annotated[List[int], Query(title="White men", description="Squares with white men")] = [],
-                      wk: Annotated[List[int], Query(title="White kings",
-                                                     description="Squares with white kings")] = []):
+async def legal_moves(
+    to_move: Annotated[
+        str,
+        Query(title="Player to move",
+              pattern=r"\b(black|white)\b",
+              description="black or white are valid selections")],
+    bm: Annotated[
+        List[int],
+        Query(title="Black men", description="Squares with black men")] = [],
+    bk: Annotated[List[int],
+                  Query(title="Black kings",
+                        description="Squares with black kings")] = [],
+    wm: Annotated[
+        List[int],
+        Query(title="White men", description="Squares with white men")] = [],
+    wk: Annotated[List[int],
+                  Query(title="White kings",
+                        description="Squares with white kings")] = []):
     sbm, sbk, swm, swk = set(bm), set(bk), set(wm), set(wk)
     if len(bm) != len(sbm):
-        raise HTTPException(status_code=422, detail="Repeated values for black men")
+        raise HTTPException(status_code=422,
+                            detail="Repeated values for black men")
     if len(bk) != len(sbk):
         raise HTTPException(status_code=422,
                             detail="Repeated values for black kings")
@@ -63,11 +74,12 @@ async def legal_moves(to_move: Annotated[str, Query(title="Player to move", patt
         captures.append(jump)
 
     moves = []
-    for move in board.curr_state.moves:
-        squares = []
-        for affected_square in move.affected_squares:
-            squares.append(keymap[affected_square[0]])
-        moves.append(squares)
+    if not captures:
+        for move in board.curr_state.moves:
+            squares = []
+            for affected_square in move.affected_squares:
+                squares.append(keymap[affected_square[0]])
+            moves.append(squares)
     captures.sort()
     moves.sort()
     return JSONResponse({"captures": captures, "moves": moves})
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,16 +5,19 @@ from main import app
 
 def test_legal_moves_success_three_kings_versus_two():
     client = TestClient(app)
-    response = client.get('/legal_moves/?to_move=white&bk=9&bk=27&wk=10&wk=15&wk=19')
+    response = client.get(
+        '/legal_moves/?to_move=white&bk=9&bk=27&wk=10&wk=15&wk=19')
     assert response.status_code == 200
-    assert response.json() == {"captures": [],
-                               "moves": [[10, 6], [10, 7], [10, 14], [15, 11], [15, 18], [19, 16], [19, 23], [19, 24]]}
+    assert response.json() == {
+        "captures": [],
+        "moves": [[10, 6], [10, 7], [10, 14], [15, 11], [15, 18], [19, 16],
+                  [19, 23], [19, 24]]
+    }
 
 
 def test_legal_captures_one_king_versus_two_breeches():
     client = TestClient(app)
-    response = client.get(
-        '/legal_moves/?to_move=white&bk=13&bk=22&wk=18')
+    response = client.get('/legal_moves/?to_move=white&bk=15&bk=22&wk=18')
     assert response.status_code == 200
     assert response.json() == {"captures": [[18, 11], [18, 25]], "moves": []}
 
