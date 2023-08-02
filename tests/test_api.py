a5aeb059754673db0fb304c9f7176bed118ab64a
Brandon Corfman <bcorfman@fastmail.fm>
2023-08-02 20:49:28 +0000

    repair test_create_session

--- tests/test_api.py
+++ tests/test_api.py
@@ -82,5 +82,11 @@ def test_create_session():
     assert response.status_code == 200
     json = response.json()
     assert json[
-        'pdn'] == "B:W21,22,23,24,25,26,27,28,29,30,31,32:B1,2,3,4,5,6,7,8,9,10,11,12"
+        'pdn'] == '[Event ""]\n' + \
+                  '[Date ""]\n' + \
+                  '[Black ""]\n' + \
+                  '[White ""]\n' + \
+                  '[Site ""]\n' + \
+                  '[Result ""]\n' + \
+                  '[BoardOrientation "white_on_top"]'
     assert json['move'] == 1
