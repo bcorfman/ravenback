c8fa6d2e0d7de1e2133cbb99e6d552aca977d312
Brandon Corfman <bcorfman@fastmail.fm>
2023-09-09 19:21:17 -0400

    Removed port 6006 config

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from util.globalconst import BLACK, KING, MAN, WHITE, keymap, square_map
 starlette_config = Config('env.txt')
 app = FastAPI()
 origins = [
-    'http://localhost:6006', 'https://localhost:6006',
     'http://localhost:6007', 'https://localhost:6007',
     'http://react-checkerboard.vercel.app',
     'https://react-checkerboard.vercel.app'
