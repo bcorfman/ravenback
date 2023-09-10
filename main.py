62b75a682aa1981ee20bc4b8d196b7d55d9075c9
Brandon Corfman <bcorfman@fastmail.fm>
2023-09-09 21:01:28 -0400

    legal_moves now uses FEN instead of multiple args

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Annotated, List, Union
+from typing import Annotated, Union
 
 from deta import Deta
 from fastapi import FastAPI, HTTPException, Query
@@ -9,7 +9,7 @@ from starlette.middleware.sessions import SessionMiddleware
 
 from game.checkers import Checkers, calc_ai_move
 from parsing.PDN import PDNReader, PDNWriter, translate_to_fen
-from util.globalconst import BLACK, KING, MAN, WHITE, keymap, square_map
+from util.globalconst import keymap
 
 starlette_config = Config('env.txt')
 app = FastAPI()
@@ -63,61 +63,22 @@ async def end_session():
 
 # example - http://localhost:8000/legal_moves?to_move=black&bm=11&bm=15&bk=19&bk=4&wm=30&wm=31&wk=29"
 @app.get("/legal_moves")
-async def legal_moves(
-    to_move: Annotated[
-        str,
-        Query(title="Player to move",
-              pattern=r"\b(black|white)\b",
-              description="black or white are valid selections")],
-    bm: Annotated[
-        List[int],
-        Query(title="Black men", description="Squares with black men")] = [],
-    bk: Annotated[List[int],
-                  Query(title="Black kings",
-                        description="Squares with black kings")] = [],
-    wm: Annotated[
-        List[int],
-        Query(title="White men", description="Squares with white men")] = [],
-    wk: Annotated[List[int],
-                  Query(title="White kings",
-                        description="Squares with white kings")] = []):
-    sbm, sbk, swm, swk = set(bm), set(bk), set(wm), set(wk)
-    if len(bm) != len(sbm):
-        raise HTTPException(status_code=422,
-                            detail="Repeated values for black men")
-    if len(bk) != len(sbk):
-        raise HTTPException(status_code=422,
-                            detail="Repeated values for black kings")
-    if len(wm) != len(swm):
-        raise HTTPException(status_code=422,
-                            detail="Repeated values for white men")
-    if len(wk) != len(swk):
-        raise HTTPException(status_code=422,
-                            detail="Repeated values for white kings")
-    if len(bm + bk + wm + wk) != len(sbm | sbk | swm | swk):
-        raise HTTPException(status_code=422,
-                            detail="Overlapping checker values")
-    if not all(1 <= e <= 32 for e in sbm | sbk | swm | swk):
-        raise HTTPException(status_code=422,
-                            detail="Valid checker values range from 1-32")
-
+async def legal_moves(fen: Annotated[
+    Union[str, None],
+    Query(title="String in Forsyth-Edwards Notation (FEN)",
+          description="If string is None, then default is starting game " +
+          "position, and black to move.")] = None):
     board = Checkers()
-    board.curr_state.clear()
-    sq = board.curr_state.squares
-    for item in wm:
-        idx = square_map[item]
-        sq[idx] = WHITE | MAN
-    for item in wk:
-        idx = square_map[item]
-        sq[idx] = WHITE | KING
-    for item in bm:
-        idx = square_map[item]
-        sq[idx] = BLACK | MAN
-    for item in bk:
-        idx = square_map[item]
-        sq[idx] = BLACK | KING
-    board.curr_state.to_move = BLACK if to_move == "black" else WHITE
-
+    state = board.curr_state
+    if fen is not None:
+        try:
+            reader = PDNReader(None)
+            game_params = reader.game_params_from_fen(fen)
+            state.setup_game(game_params)
+        except (SyntaxError, RuntimeError) as e:
+            # exception message held in first element of args list
+            raise HTTPException(status_code=422, detail=e.args[0])
+        
     captures = []
     for capture in board.curr_state.captures:
         jump = []
@@ -145,20 +106,8 @@ async def get_checkerboard_state():
     if not result:
         return JSONResponse(status_code=404,
                             content={'message': 'Session not found.'})
-    reader = PDNReader(None)
-    game_params = reader.game_params_from_fen(result['fen'])
-    board = Checkers()
-    state = board.curr_state
-    state.setup_game(game_params)
-    next_to_move, black_men, black_kings, white_men, white_kings = state.save_board_state(
-    )
-
     return JSONResponse({
-        'to_move': next_to_move,
-        'black_men': black_men,
-        'black_kings': black_kings,
-        'white_men': white_men,
-        'white_kings': white_kings
+        'fen': result['fen']
     })
 
 
